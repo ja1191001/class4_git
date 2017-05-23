d1e8860274d21723864662ab076ed09c2aeaa493
user <user@user-PC>
2017-05-23 11:49:33 +0900

    zeus 수정

--- test_git/src/kr/co/sist/test/test.java
+++ test_git/src/kr/co/sist/test/test.java
@@ -5,6 +5,8 @@ public class test {
 	public static void main(String[] args) {
 		int i=0;
 		System.out.println("변수 값 i = "+i);
+		//zeus 가 수정
+		System.out.println("i'm Zeus");
 	}//main
 
 }//class
